26e155eeea462571c0f53cbd813e2ec7c9d86c13
dualdragoon <hosleraaron06@gmail.com>
2015-09-25 10:19:23 -0400

    Initial

---
+++ Unnamed_Racing_Game/Main.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Duality;
+using SharpDX;
+using SharpDX.Toolkit;
+using SharpDX.Toolkit.Content;
+using SharpDX.Toolkit.Graphics;
+using SharpDX.Toolkit.Input;
+
+namespace Unnamed_Racing_Game
+{
+    class Main : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spritebatch;
+
+        public static MouseState CurrentMouse
+        {
+            get { return mouse; }
+            set { mouse = value; }
+        }
+        private static MouseState mouse;
+
+        public static KeyboardState CurrentKeyboard
+        {
+            get { return keyboard; }
+            set { keyboard = value; }
+        }
+        private static KeyboardState keyboard;
+
+        public static MouseManager Mouse
+        {
+            get { return mouseManager; }
+        }
+        private static MouseManager mouseManager;
+
+        public static KeyboardManager Keyboard
+        {
+            get { return keyboardManager; }
+        }
+        private static KeyboardManager keyboardManager;
+
+        public Main()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            mouseManager = new MouseManager(this);
+            keyboardManager = new KeyboardManager(this);
+        }
+
+        protected override void Initialize()
+        {
+            ErrorHandler.Initialize();
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spritebatch = new SpriteBatch(GraphicsDevice);
+
+            base.LoadContent();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            
+            
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            Window.AllowUserResizing = false;
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Unnamed_Racing_Game/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpDX.Toolkit;
+
+namespace Unnamed_Racing_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (Main game = new Main())
+            {
+                game.Run();
+            }
+        }
+    }
+}
