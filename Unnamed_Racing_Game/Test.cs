86f2f18af9ba79baa3c8e2f44ce610c2c94c2697
dualdragoon <hosleraaron06@gmail.com>
2015-10-26 09:41:01 -0400

    BLARRGH

--- Unnamed_Racing_Game/Test.cs
+++ Unnamed_Racing_Game/Test.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 using SharpDX;
 using SharpDX.Toolkit;
 using SharpDX.Toolkit.Content;
@@ -16,7 +17,8 @@ namespace Kross_Kart
         float angle = 0;
         Matrix translation, view, projection;
         Model sphere;
-        Vector3 position = new Vector3(0, -1, 0);
+        Stopwatch s = new Stopwatch();
+        Vector3 position = new Vector3(0, -1, 0), temp;
 
         public Test()
         {
@@ -35,9 +37,21 @@ namespace Kross_Kart
             this.view = view;
             this.projection = projection;
 
+            if (!s.IsRunning)
+            {
+                s.Start(); 
+            }
+
             angle += .03f;
-            position += new Vector3(0, -.01f, 0);
-            translation = Matrix.Translation(position) * Matrix.RotationAxis(position, angle);
+            position += new Vector3(-.01f, -.01f, 0);
+            temp = new Vector3(position.ToArray());
+            temp.Normalize();
+            translation = Matrix.Translation(position) * Matrix.RotationAxis(temp, MathUtil.DegreesToRadians(s.ElapsedMilliseconds));
+
+            if (s.ElapsedMilliseconds == 360)
+            {
+                s.Restart();
+            }
 
             if (Main.CurrentKeyboard.IsKeyPressed(Keys.A))
             {
